9925ea3530e2203afdbe253e4b85b35e65362784
ZhenhuiHe15 <herbertynwa@outlook.com>
2024-06-02 10:57:01 +1000

    Slider Page(image item)

--- client/src/routes/singlePage/singlePage.jsx
+++ client/src/routes/singlePage/singlePage.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import Slider from '../../components/slider/slider';
-import { singlePostData } from '../../lib/dummyDB';
-
+import { singlePostData, userData } from '../../lib/dummyDB';
+import './singlePage.scss';
 function SinglePage() {
   return (
     <div className='singlePage'>
       <div className='details'>
         <div className='wrapper'>
-          <Slider imges={singlePostData.images} />
+          <Slider images={singlePostData.images} />
           <div className='info'>
             <div className='top'>
-              <div className='post'></div>
-              <div className='user'></div>
+              <div className='post'>
+                <h1>{singlePostData.title}</h1>
+                <div className='address'>
+                  <img src='/pin.png' alt='' />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className='price'>$ {singlePostData.price}</div>
+              </div>
+              <div className='user'>
+                <img src={userData.img} alt='' />
+                <span>{userData.name}</span>
+              </div>
             </div>
-            <div className='bottom'></div>
+            <div className='bottom'>{singlePostData.description}</div>
           </div>
         </div>
       </div>
