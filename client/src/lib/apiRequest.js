7817e5aa340a194a4182c5f096e445ac58c51190
ZhenhuiHe15 <herbertynwa@outlook.com>
2024-07-09 23:05:50 +1000

    test deploy

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiRequest = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // 使用环境变量
+  baseURL: 'https://react-real-estate-o5wr.vercel.app/',
   withCredentials: true,
 });
 
