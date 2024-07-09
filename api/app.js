758dd1c94a8a6ff206487dc35f15ee2afc47b8d8
ZhenhuiHe15 <herbertynwa@outlook.com>
2024-07-09 21:52:18 +1000

    test for deploy

--- api/app.js
+++ api/app.js
@@ -6,28 +6,21 @@ import testRouter from './routes/test.route.js';
 import userRouter from './routes/user.route.js';
 import chatRouter from './routes/chat.route.js';
 import messageRouter from './routes/message.route.js';
-
 import cookieParser from 'cookie-parser';
+
 const app = express();
 
-//middleware
+// Middleware
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-//create a api endpoint
 
+// Create API endpoints
 app.use('/api/users', userRouter);
-
 app.use('/api/posts', postRouter);
-
 app.use('/api/auth', authRouter);
-
 app.use('/api/test', testRouter);
-
 app.use('/api/chats', chatRouter);
-
 app.use('/api/messages', messageRouter);
 
-app.listen(8800, () => {
-  console.log('Server is running');
-});
+export default app; // Export the app for Vercel
