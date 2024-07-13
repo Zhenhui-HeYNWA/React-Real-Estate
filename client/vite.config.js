bd6a8b4bcc9275a22a1e156cd23d79d9ee5eb804
ZhenhuiHe15 <herbertynwa@outlook.com>
2024-07-14 02:36:06 +1000

    test1

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../client/build',
+    outDir: 'build', // This should be inside the client directory
   },
   server: {
     port: 3000,
