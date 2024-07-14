95b9050df8e4c1f0db929837f447fbea363b70e4
ZhenhuiHe15 <herbertynwa@outlook.com>
2024-07-14 10:48:10 +1000

    change CLIENT_URL

--- api/server.js
+++ api/server.js
@@ -11,7 +11,21 @@ import cookieParser from 'cookie-parser';
 import path from 'path';
 
 const app = express();
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+const allowedOrigins = [process.env.CLIENT_URL];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
